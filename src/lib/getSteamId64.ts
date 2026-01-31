4dd204df2a713b9ba109d355429ead1cbd180a1a
Junnian Liu <liuj4467@Junnians-MacBook-Air.local>
2026-01-31 15:26:24 -0800

    fixed working tree

---
+++ src/lib/getSteamId64.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verifyAuthJwt } from "./auth";
+
+export function getSteamId64(req: NextRequest) {
+    const token = req.cookies.get("auth")?.value;
+    if (!token) {
+        throw new Error("Unauthorized");
+    }
+
+    const payload = verifyAuthJwt(token);
+    const steamId64 = payload.steamId64;
+
+    return steamId64;
+}
